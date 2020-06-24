11847a10c38550ddcd4573a512e1b75047fc228d
augustineritalondon <augustineritalondon@gmail.com>
2020-06-24 21:19:08 +0000

    menu page

--- app.py
+++ app.py
@@ -11,5 +11,13 @@ def home():
 def menu():
     return render_template("menu.html")
 
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+@app.route('/contact')
+def contact():
+    return render_template("contact.html")
+
 if __name__=='__main__':
     app.run(debug=True)
